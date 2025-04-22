fcbcc30dfdc37be09e9f0384e6f7eeadf6ad1b6d
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-04-22 11:54:32 +0900

    インチからメートルに変換完成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintInchToMeterList(1, 10);
+            } else {
+                PrintMeterToInchList(1, 10);
+            }
+
+            //フィートからメートルへの対応表を出力
+            static void PrintInchToMeterList(int start, int end) {
+                for (int inch = start; inch <= end; inch++) {
+                    double meter = inchConverter.ToMeter(inch);
+                    Console.WriteLine($"{inch} inch = {meter:0.0000}m");
+                }
+            }
+
+            //メートルからフィートへの対応表を出力
+            static void PrintMeterToInchList(int start, int end) {
+                for (int meter = start; meter <= end; meter++) {
+                    double inch = inchConverter.FromMeter(meter);
+                    Console.WriteLine($"{meter}m = {inch:0.0000}inch");
+                }
+            }
+
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/inchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class inchConverter {
+
+        private const double ratio = 0.0254;
+
+        //メートルからフィートを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
