1d239ab6ca007fa260efc26084766317bf837f76
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-05-14 11:11:49 +0900

    0514演習 出力

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Exercise01 {
+    public class Program {
+        static void Main(string[] args) {
+            //2.1.3
+
+            var songs = new List<Song>();
+
+
+            //曲の登録を出力
+            Console.Write("****** 曲の登録 ******");
+
+            while (true) {
+                //曲名出力
+                Console.Write("曲名:");
+                //入力された曲名取得
+                string? title = Console.ReadLine();
+
+                //endが入力されたら終了
+                if (title.Equals("end", StringComparison.OrdinalIgnoreCase))
+                    return;
+
+                //アーティスト出力
+                Console.Write("アーティスト名：");
+                //入力されたアーティスト取得
+                string? artistName = Console.ReadLine();
+
+                //演奏時間出力
+                Console.Write("演奏時間（秒）：");
+                //入力された演奏時間取得
+                int length = int.Parse(Console.ReadLine());
+
+                //Songインスタンス生成
+                //Song song = new Song(title, artistName, length);
+
+                Song song = new Song() {
+                    Title = title,
+                    ArtistName = artistName,
+                    Length = length
+                };
+
+                //歌データ登録
+                songs.Add(song);
+
+                Console.WriteLine();  //改行
+
+
+            }
+            printSongs(songs);
+        }
+        
+        //2.1.4
+        private static void printSongs(List<Song> songs) {
+            foreach (var song in songs) {
+                var minutes = song.Length / 60;
+                var seconds = song.Length % 60;
+                Console.WriteLine($"{song.Title}, {song.ArtistName} {minutes}:{seconds:00}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
