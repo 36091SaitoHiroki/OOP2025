00c087fbf0eaa1ffef87ca3abdd825e1a3c404aa
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-05-30 15:33:36 +0900

    6.3.1完成

---
+++ Chapter06/演習問題/Exercose01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            Console.WriteLine();
+            var cultureinfo = new CultureInfo("ja-JP");
+            if (String.Compare(str1, str2, cultureinfo, CompareOptions.IgnoreKanaType) == 0) 
+                Console.WriteLine("一致しています");
+            else 
+                Console.WriteLine("一致していない");
+            
+
+        }
+    }
+}
