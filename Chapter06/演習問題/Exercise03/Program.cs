c2084424f711c2deb04568b6a7bfca4755af6808
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-06-04 11:27:49 +0900

    6.3.6完成

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -5,6 +5,7 @@ namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
+
             #region
             Console.WriteLine("6.3.1");
             Exercise1(text);
@@ -20,6 +21,9 @@ namespace Exercise03 {
 
             Console.WriteLine("6.3.5");
             Exercise5(text);
+
+            Console.WriteLine("6.3.6");
+            Exercise6(text);
             #endregion
         }
 
@@ -52,7 +56,46 @@ namespace Exercise03 {
             var words = text.Split(' ').Where(s => s.Length <= 4);
             foreach (var word in words) {
                 Console.WriteLine(word);
-            } 
+            }
+        }
+
+        private static void Exercise6(string text) {
+            var str = text.ToLower().Replace(" ","");
+
+            //辞書で集計
+            var alphDicCount = Enumerable.Range('a', 26)
+                                .ToDictionary(num => ((char)num).ToString(),num => 0);
+            foreach (var alph in str) {
+                    alphDicCount[alph.ToString()]++;
+            }
+
+            foreach (var item in alphDicCount) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            Console.WriteLine();
+            //*****************************************//
+            //配列で集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+
+            foreach (var alph in str) {
+                array[alph - 'a']++;
+            }
+
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
+
+            Console.WriteLine();
+            //*****************************************//
+            //aから順にカウントして出力
+
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{text.Count(tc => tc == ch)}");
+            }
+
+
+
         }
     }
 }
