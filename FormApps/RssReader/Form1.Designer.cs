1c611c313ed92191d6e7ebb10d3c2c4a2c9ab713
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-07-22 14:00:54 +0900

    戻る・進むボタン完成

---
+++ FormApps/RssReader/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tbUrl = new TextBox();
+            btRssGet = new Button();
+            lbTitles = new ListBox();
+            wvRssLink = new Microsoft.Web.WebView2.WinForms.WebView2();
+            GoBack = new Button();
+            GoForward = new Button();
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).BeginInit();
+            SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            tbUrl.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            tbUrl.Location = new Point(124, 14);
+            tbUrl.Name = "tbUrl";
+            tbUrl.Size = new Size(498, 33);
+            tbUrl.TabIndex = 0;
+            // 
+            // btRssGet
+            // 
+            btRssGet.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btRssGet.Location = new Point(628, 14);
+            btRssGet.Name = "btRssGet";
+            btRssGet.Size = new Size(83, 33);
+            btRssGet.TabIndex = 1;
+            btRssGet.Text = "取得";
+            btRssGet.UseVisualStyleBackColor = true;
+            btRssGet.Click += btRssGet_Click;
+            // 
+            // lbTitles
+            // 
+            lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbTitles.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            lbTitles.FormattingEnabled = true;
+            lbTitles.ItemHeight = 21;
+            lbTitles.Location = new Point(12, 91);
+            lbTitles.Name = "lbTitles";
+            lbTitles.Size = new Size(696, 172);
+            lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // wvRssLink
+            // 
+            wvRssLink.AllowExternalDrop = true;
+            wvRssLink.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            wvRssLink.CreationProperties = null;
+            wvRssLink.DefaultBackgroundColor = Color.White;
+            wvRssLink.Location = new Point(13, 269);
+            wvRssLink.Name = "wvRssLink";
+            wvRssLink.Size = new Size(696, 354);
+            wvRssLink.TabIndex = 3;
+            wvRssLink.ZoomFactor = 1D;
+            wvRssLink.SourceChanged += wvRssLink_SourceChanged;
+            // 
+            // GoBack
+            // 
+            GoBack.Location = new Point(14, 14);
+            GoBack.Name = "GoBack";
+            GoBack.Size = new Size(46, 33);
+            GoBack.TabIndex = 4;
+            GoBack.Text = "戻る";
+            GoBack.UseVisualStyleBackColor = true;
+            GoBack.Click += GoBack_Click;
+            // 
+            // GoForward
+            // 
+            GoForward.Location = new Point(66, 15);
+            GoForward.Name = "GoForward";
+            GoForward.Size = new Size(52, 32);
+            GoForward.TabIndex = 5;
+            GoForward.Text = "進む";
+            GoForward.UseVisualStyleBackColor = true;
+            GoForward.Click += GoForward_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(724, 646);
+            Controls.Add(GoForward);
+            Controls.Add(GoBack);
+            Controls.Add(wvRssLink);
+            Controls.Add(lbTitles);
+            Controls.Add(btRssGet);
+            Controls.Add(tbUrl);
+            Name = "Form1";
+            Text = "RSSリーダー";
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbUrl;
+        private Button btRssGet;
+        private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wvRssLink;
+        private Button GoBack;
+        private Button GoForward;
+    }
+}
