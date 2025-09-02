f642ab3710d0a76df94e16312d2e68907ea82ff3
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-09-02 13:23:18 +0900

    11演習問題

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var strings = new[] {
+                "Microsoft Windows",
+                "windows",
+                "Windows Server",
+                "Windows",
+            };
+
+            var regex = new Regex(@"^(W|w)indows$");
+
+            var count = strings.Count(s => regex.IsMatch(s));
+            Console.WriteLine($"{count}行と一致");
+
+            //パターンと完全に一致している文字列
+
+            var datas = strings.Where(s => regex.IsMatch(s));
+            foreach (var item in datas) {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
