66823d1c22ed33cc1e61b98f81d528734088c5c1
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-10-09 14:15:24 +0900

    単位変更アプリ完成

---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter{
+    internal class MainWindowViewModel : ViewModel{
+
+        //フィールド
+        private double metricValue;
+        private double imperiaValue;
+
+        public ICommand ImperialUnitToMetric { get;private set; }
+
+        public ICommand MetricToImperialUnit { get; private set; }
+
+        public MetricUnit CurrentMetricUnit { get; set; }
+
+        public ImperialUnit CurrentImperialUnit { get; set; }
+
+        //プロパティ
+        public double MetricValue { 
+            get => metricValue; 
+            set {
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public double ImperiaValue { 
+            get => imperiaValue; 
+            set {
+                this.imperiaValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel() {
+            CurrentMetricUnit = MetricUnit.Units.First();
+            CurrentImperialUnit = ImperialUnit.Units.First();
+
+
+            ImperialUnitToMetric = new DelegateCommand(
+                () => MetricValue = CurrentMetricUnit.FromImperialUnit(
+                    CurrentImperialUnit,ImperiaValue));
+
+            MetricToImperialUnit = new DelegateCommand(
+                () => ImperiaValue = CurrentImperialUnit.FromMetriclUnit(
+                    CurrentMetricUnit,MetricValue));
+        }
+    }
+}
