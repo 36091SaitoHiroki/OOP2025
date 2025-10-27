e09437a26bea3c007cf43fda8abacd07c976d85a
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-10-27 15:53:18 +0900

    ２完成

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //var price = Library.Books
+            //    .Where(b => b.CategoryId == 1)
+            //    .Max(b => b.Price);
+            //Console.WriteLine(price);
+
+            //Console.WriteLine();
+
+            //var book = Library.Books
+            //    .Where(b => b.PublishedYear >= 2021)
+            //    .MinBy(b => b.Price);
+            //Console.WriteLine(book);
+
+            //Console.WriteLine();
+
+            //var average = Library.Books.Average(x => x.Price);
+            //var aboves = Library.Books
+            //    .Where(b => b.Price > average);
+            //foreach (var book1 in aboves) {
+            //    Console.WriteLine(book1);
+            //}
+
+            var selected = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(group=>group.MaxBy(b=>b.Price))
+                .OrderBy(b=>b!.PublishedYear);
+            foreach (var book in selected) {
+                Console.WriteLine($"{book!.PublishedYear}年{book!.Title}({book!.Price})");
+            }
+
+        }
+    }
+}
---
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+
+        }
+
+        private static void Exercise1_2() {
+            var price = Library.Books
+                .Where(b => b.CategoryId == 1)
+                .MaxBy(b => b.Price);
+
+            Console.WriteLine(price);
+        }
+
+        private static void Exercise1_3() {
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
