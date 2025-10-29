a983c31f180dc58892c09655772f1d17cb9bc8a5
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-10-29 10:56:14 +0900

    1_5

--- Chapter13/Section01/Program.cs
+++ Chapter13/Section01/Program.cs
@@ -7,14 +7,12 @@
             //    .Max(b => b.Price);
             //Console.WriteLine(price);
 
-            //Console.WriteLine();
 
             //var book = Library.Books
             //    .Where(b => b.PublishedYear >= 2021)
             //    .MinBy(b => b.Price);
             //Console.WriteLine(book);
 
-            //Console.WriteLine();
 
             //var average = Library.Books.Average(x => x.Price);
             //var aboves = Library.Books
@@ -23,14 +21,31 @@
             //    Console.WriteLine(book1);
             //}
 
-            var selected = Library.Books
-                .GroupBy(b => b.PublishedYear)
-                .Select(group=>group.MaxBy(b=>b.Price))
-                .OrderBy(b=>b!.PublishedYear);
-            foreach (var book in selected) {
-                Console.WriteLine($"{book!.PublishedYear}年{book!.Title}({book!.Price})");
-            }
 
+            //var selected = Library.Books
+            //    .GroupBy(b => b.PublishedYear)
+            //    .Select(group=>group.MaxBy(b=>b.Price))
+            //    .OrderBy(b=>b!.PublishedYear);
+            //foreach (var book in selected) {
+            //    Console.WriteLine($"{book!.PublishedYear}年{book!.Title}({book!.Price})");
+            //}
+
+
+            var books = Library.Books
+                               .Join(Library.Categories
+                                     , book => book.CategoryId
+                                     , Category => Category.Id
+                                     , (book, category) => new {
+                                         book.Title,
+                                         Category = category.Name,
+                                         book.PublishedYear
+                                     })
+                               .OrderBy(b=>b.PublishedYear)
+                               .ThenBy(b=>b.Category);
+
+            foreach (var book in books) {
+                Console.WriteLine($"{book.Title},{book.Category},{book.PublishedYear}");
+            }
         }
     }
 }
--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -22,23 +22,49 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2() {
-            var price = Library.Books
-                .Where(b => b.CategoryId == 1)
-                .MaxBy(b => b.Price);
+            var book = Library.Books
+                              .MaxBy(b => b.Price);
 
-            Console.WriteLine(price);
+            Console.WriteLine(book);
         }
 
         private static void Exercise1_3() {
+            var results = Library.Books
+                                 .GroupBy(b => b.PublishedYear)
+                                 .OrderBy(b=>b.Key)
+                                 .Select(b => new {
+                                     PublishedYear = b.Key,
+                                     Count = b.Count()
+                                 });
+
+            foreach (var item in results) {
+                Console.WriteLine($"{item.PublishedYear}:{item.Count}");
+            }
             
         }
 
         private static void Exercise1_4() {
-            
+            var book = Library.Books
+                              .OrderByDescending(b => b.Price)
+                              .ThenByDescending(b => b.PublishedYear);
+            foreach (var item in book) {
+                Console.WriteLine($"{item.PublishedYear}年 {item.Price}円 {item.Title}");
+            }
         }
 
         private static void Exercise1_5() {
-            
+            var book = Library.Books
+                              .Where(b => b.PublishedYear == 2022)
+                              .Join(Library.Categories,
+                                book => book.CategoryId,
+                                category => category.Id,
+                                (book, category) => category.Name)
+                              .Distinct();
+            foreach (var books in book) {
+                Console.WriteLine(books);
+            }
+                              
+                              
         }
 
         private static void Exercise1_6() {
