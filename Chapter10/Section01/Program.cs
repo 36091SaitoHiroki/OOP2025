8d6f20ce286ff4a25f11ed92553653e66c2fd813
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-11-10 13:37:06 +0900

    10.1

---
+++ Chapter10/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //コード10.1  テキストファイルを一行ずつ読み込む
+
+            var filePath = "./Greeting.txt";
+            if (File.Exists(filePath)) {
+                using var reader = new StreamReader(filePath, encoding.UTF8);
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+
+        }
+    }
+}
