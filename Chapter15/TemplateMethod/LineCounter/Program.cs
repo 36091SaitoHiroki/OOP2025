8e407de31fd03980fae3eb1f00fa778593cd138b
36091SaitoHiroki <oibc36091@ojs.ac.jp>
2025-11-12 11:32:00 +0900

    LineCounterProcessor

---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using TextFileProcessor;
+
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<LineCounterProcessor>(args[0]);
+
+            
+
+        }
+    }
+}
